59d50527cf5fa5cc270b29b08e9814e4fe971744
kklai <kkrebeccalai@gmail.com>
2014-07-31 18:12:53 -0400

    defining input is messing it up'

--- source/javascripts/getdata.js
+++ source/javascripts/getdata.js
@@ -1,98 +1,100 @@
-var input, key, pub, quizType;
+(function ($) {
+  var key, pub, quizType;
 
-// initialize tabletop library
-function init() {
-		Tabletop.init( { key: url,
-                   callback: readData,
-                   simpleSheet: true } );
-	}
+  // initialize tabletop library
+  function init() {
+  		Tabletop.init( { key: url,
+                     callback: readData,
+                     simpleSheet: true } );
+  	}
 
-function readData(data, tabletop) {
-	input = [];
-	for ( var i = 0; i < data.length; i++ ) {
-		input[i] = findUrlinObject( data[i] );
-	}
-	addJS();
-	embed();
-}
-
-function findUrlinObject ( data ) {
-	$.each( data, function( key, value ){
-		if ( key == 'correct' || key == 'incorrect' ) {
-			data[key] = converttoHex( data[key] );
-		}
-	} );
-	return data;
-}
-
-function converttoHex ( string ) {
-	var hex, i;
-	var result = "";
-	for ( i = 0; i < string.length; i++ ) {
-		hex = string.charCodeAt( i ).toString( 16 );
-		result += ( "000" + hex ).slice( -4 );
-	}
-	return result;
-}
-
-function addJS() {
-	quizType = $('input[name="quiz-type"]:checked').val();
-		if (quizType == 'quiz') {
-			$('body').append('<script src="/javascripts/quiz.js" type="text/javascript"><\/script>');
-		} else {
-			$('body').append('<script src="/javascripts/flowchart.js" type="text/javascript"><\/script>');
-		}
-}
-
-function changeTemplate() {
-	if (quizType == 'quiz') {
-    $('#quiz-template').attr('href', 'https://drive.google.com/previewtemplate?id=0AlMgrVuuAI0MdGl6NngwMGYtX3RHQjlic0xzNnBjUGc&mode=public').addClass('template');
-    $('#example-spreadsheet').val('https://docs.google.com/spreadsheet/pub?key=0AlMgrVuuAI0MdGl6NngwMGYtX3RHQjlic0xzNnBjUGc&output=html');
-  } else {
-    $('#quiz-template').attr('href', 'https://drive.google.com/previewtemplate?id=0AlMgrVuuAI0MdE9ZNVhnYmk0TUdidGhiZTgwT0F6MGc&mode=public').addClass('template');
-    $('#example-spreadsheet').val('https://docs.google.com/spreadsheet/pub?key=0ArcRX35HpjojdGlSR012UjVDZkpIM19ObVY5TE03U2c&output=html');
+  function readData(data, tabletop) {
+  	input = [];
+  	for ( var i = 0; i < data.length; i++ ) {
+  		input[i] = findUrlinObject( data[i] );
+  	}
+  	addJS();
+  	embed();
   }
-}
 
-function submitquiz() {
-	quizType = $('input[name="quiz-type"]:checked').val();
-	if (quizType == 'quiz') {
-		$('.quiz-container').empty();
-		buildquiz();
-	} else {
-		$('.quiz-container').empty();
-		buildflowchart();
-	}
-}
+  function findUrlinObject ( data ) {
+  	$.each( data, function( key, value ){
+  		if ( key == 'correct' || key == 'incorrect' ) {
+  			data[key] = converttoHex( data[key] );
+  		}
+  	} );
+  	return data;
+  }
 
-function getStylesheet() {
-	pub = $('input[name="pub"]:checked').val();
-  pubStylesheet = "http://assets.sbnation.com.s3.amazonaws.com/features/quiz-generator/quiz-" + pub + ".css";
-  // pubStylesheet = "/stylesheets/quiz-" + pub + ".css";
-}
+  function converttoHex ( string ) {
+  	var hex, i;
+  	var result = "";
+  	for ( i = 0; i < string.length; i++ ) {
+  		hex = string.charCodeAt( i ).toString( 16 );
+  		result += ( "000" + hex ).slice( -4 );
+  	}
+  	return result;
+  }
 
-function embed() {
-  $("#embedcode").html("&lt;div class='quiz-container'></div>&lt;script type='text/javascript'>window.jQuery || document.write('&lt;script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>&lt;&#92;/script>');&lt;/script>&lt;script type='text/javascript'>var input = " + JSON.stringify(input) + "; var pubStylesheet = '" + pubStylesheet + "'; var pub = '" + pub + "'; &lt;/script>&lt;script src='http://assets.sbnation.com.s3.amazonaws.com/features/quiz-generator/" + quizType + ".js'>&lt;/script>");
-  // $("#embedcode").html("&lt;div class='quiz-container'></div>&lt;script type='text/javascript'>window.jQuery || document.write('&lt;script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>&lt;&#92;/script>');&lt;/script>&lt;script type='text/javascript'>var input = " + JSON.stringify(input) + "; var pubStylesheet = '" + pubStylesheet + "';&lt;/script>&lt;script src='/javascripts/" + quizType + ".js'>&lt;/script>");
-}
+  function addJS() {
+  	quizType = $('input[name="quiz-type"]:checked').val();
+  		if (quizType == 'quiz') {
+  			$('body').append('<script src="/javascripts/quiz.js" type="text/javascript"><\/script>');
+  		} else {
+  			$('body').append('<script src="/javascripts/flowchart.js" type="text/javascript"><\/script>');
+  		}
+  }
 
-function buildquiz(){
-  url = $('#url').val();
-  init();
-  getStylesheet();
-}
+  function changeTemplate() {
+  	if (quizType == 'quiz') {
+      $('#quiz-template').attr('href', 'https://drive.google.com/previewtemplate?id=0AlMgrVuuAI0MdGl6NngwMGYtX3RHQjlic0xzNnBjUGc&mode=public').addClass('template');
+      $('#example-spreadsheet').val('https://docs.google.com/spreadsheet/pub?key=0AlMgrVuuAI0MdGl6NngwMGYtX3RHQjlic0xzNnBjUGc&output=html');
+    } else {
+      $('#quiz-template').attr('href', 'https://drive.google.com/previewtemplate?id=0AlMgrVuuAI0MdE9ZNVhnYmk0TUdidGhiZTgwT0F6MGc&mode=public').addClass('template');
+      $('#example-spreadsheet').val('https://docs.google.com/spreadsheet/pub?key=0ArcRX35HpjojdGlSR012UjVDZkpIM19ObVY5TE03U2c&output=html');
+    }
+  }
 
-function buildflowchart() {
-  url = $('#url').val();
-  init();
-  getStylesheet();
-}
+  function submitquiz() {
+  	quizType = $('input[name="quiz-type"]:checked').val();
+  	if (quizType == 'quiz') {
+  		$('.quiz-container').empty();
+  		buildquiz();
+  	} else {
+  		$('.quiz-container').empty();
+  		buildflowchart();
+  	}
+  }
 
-$(document).ready(function() {
-  $('input:radio[name=quiz-type]').click(function() {
-    quizType = $('input:radio[name=quiz-type]:checked').val();
-    changeTemplate();
-  });
+  function getStylesheet() {
+  	pub = $('input[name="pub"]:checked').val();
+    pubStylesheet = "http://assets.sbnation.com.s3.amazonaws.com/features/quiz-generator/quiz-" + pub + ".css";
+    // pubStylesheet = "/stylesheets/quiz-" + pub + ".css";
+  }
 
-  $('#build').on('click', submitquiz)
-})
+  function embed() {
+    $("#embedcode").html("&lt;div class='quiz-container'></div>&lt;script type='text/javascript'>window.jQuery || document.write('&lt;script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>&lt;&#92;/script>');&lt;/script>&lt;script type='text/javascript'>var input = " + JSON.stringify(input) + "; var pubStylesheet = '" + pubStylesheet + "'; var pub = '" + pub + "'; &lt;/script>&lt;script src='http://assets.sbnation.com.s3.amazonaws.com/features/quiz-generator/" + quizType + ".js'>&lt;/script>");
+    // $("#embedcode").html("&lt;div class='quiz-container'></div>&lt;script type='text/javascript'>window.jQuery || document.write('&lt;script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>&lt;&#92;/script>');&lt;/script>&lt;script type='text/javascript'>var input = " + JSON.stringify(input) + "; var pubStylesheet = '" + pubStylesheet + "';&lt;/script>&lt;script src='/javascripts/" + quizType + ".js'>&lt;/script>");
+  }
+
+  function buildquiz(){
+    url = $('#url').val();
+    init();
+    getStylesheet();
+  }
+
+  function buildflowchart() {
+    url = $('#url').val();
+    init();
+    getStylesheet();
+  }
+
+  $(document).ready(function() {
+    $('input:radio[name=quiz-type]').click(function() {
+      quizType = $('input:radio[name=quiz-type]:checked').val();
+      changeTemplate();
+    });
+
+    $('#build').on('click', submitquiz)
+  })
+})(jQuery);
