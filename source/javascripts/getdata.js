3f369915587de3a5a08d87a2bd105798f18fc2f4
nicolezhu <nzhu94@gmail.com>
2014-08-18 11:29:29 -0400

    fixed verge stylesheet and embed links

--- source/javascripts/getdata.js
+++ source/javascripts/getdata.js
@@ -68,12 +68,12 @@
   function getStylesheet() {
   	pub = $('input[name="pub"]:checked').val();
     $('body').append('<script type="text/javascript">var pub ="' + pub + '"</script>');
-    pubStylesheet = "http://cdn0.sbnation.com/features/quiz-generator/quiz-" + pub + ".css";
+    pubStylesheet = "http://assets.sbnation.com.s3.amazonaws.com/features/quiz-generator/quiz-" + pub + ".css";
     // pubStylesheet = "/stylesheets/quiz-" + pub + ".css";
   }
 
   function embed(input) {
-    $("#embedcode").html("&lt;div class='quiz-container'></div>&lt;script type='text/javascript'>window.jQuery || document.write('&lt;script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>&lt;&#92;/script>');&lt;/script>&lt;script type='text/javascript'>var input = " + JSON.stringify(input) + "; var pubStylesheet = '" + pubStylesheet + "'; var pub = '" + pub + "'; &lt;/script>&lt;script src='http://cdn0.sbnation.com/features/quiz-generator/" + quizType + ".js'>&lt;/script>");
+    $("#embedcode").html("&lt;div class='quiz-container'></div>&lt;script type='text/javascript'>window.jQuery || document.write('&lt;script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>&lt;&#92;/script>');&lt;/script>&lt;script type='text/javascript'>var input = " + JSON.stringify(input) + "; var pubStylesheet = '" + pubStylesheet + "'; var pub = '" + pub + "'; &lt;/script>&lt;script src='http://assets.sbnation.com.s3.amazonaws.com/features/quiz-generator/" + quizType + ".js'>&lt;/script>");
     // $("#embedcode").html("&lt;div class='quiz-container'></div>&lt;script type='text/javascript'>window.jQuery || document.write('&lt;script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>&lt;&#92;/script>');&lt;/script>&lt;script type='text/javascript'>var input = " + JSON.stringify(input) + "; var pubStylesheet = '" + pubStylesheet + "';&lt;/script>&lt;script src='/javascripts/" + quizType + ".js'>&lt;/script>");
     addJS();
   }
